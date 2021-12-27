608f0b5b8ba4bd9563695d3c0586d41bcefd8b9a
Piuavlr <63988968+Piposo745@users.noreply.github.com>
2021-12-27 17:17:41 -0300

    Att InvestirBolsa

--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+mport java.util.Random;
 import java.util.Scanner;
 
 public abstract class Conta implements IConta {
@@ -9,21 +9,26 @@ public abstract class Conta implements IConta {
     protected static final int AGENCIA_PADRAO = 001;
     protected int escolha;
     protected int investimentoVariavel;
+    protected int investBolsa;
+    protected int subiuOuDesceu;
+    protected boolean vender;
     Scanner digite = new Scanner(System.in);
     Random investimento = new Random();
+    Random bolsa = new Random();
+
 
     public void investimentos(){
         System.out.println("Digite 1 para renda fixa ou 2 para renda variavel");
         escolha = digite.nextInt();
-        investimentoVariavel = investimento.nextInt(1,4);
+        investimentoVariavel = investimento.nextInt(3);
 
         if (escolha == 1){
             this.saldo *= 1.03f;
         } else {
-            if (investimentoVariavel == 1){
+            if (investimentoVariavel == 0){
                 this.saldo *= 1.05f;
                 System.out.println("+ 5% na carteira");
-            } else if (investimentoVariavel == 2){
+            } else if (investimentoVariavel == 1){
                 this.saldo *= 1.15f;
                 System.out.println("+ 15% na carteira");
             } else {
@@ -74,6 +79,26 @@ public abstract class Conta implements IConta {
         System.out.println("Conta: "+this.numeroConta);
 
     }
+
+
+    public void investirBolsa(){
+        System.out.println("Você acha que a ação vai subir ou descer? [0 - Subiu ou 1 - Desceu]");
+        subiuOuDesceu = digite.nextInt();
+        investBolsa = bolsa.nextInt(2);
+        if (subiuOuDesceu == 0 && investBolsa == 0){
+            this.saldo *= 1.33f;
+            System.out.println("Você acertou a ação subiu, isso te rendeu 33% de lucro UAU% :))");
+        } else if (subiuOuDesceu == 1 && investBolsa == 1){
+            this.saldo *= 1.09f;
+            System.out.println("Você acertou a ação desceu e isso te rendeu 9% de lucro");
+        } else {
+            this.saldo *= 0.82;
+            System.out.println("Que desastre você errou :(, descanse e tente mais tarde ou peça um emprestimo conosco :)");
+        }
+    }
+
+
+
 
     public int getAgencia() {
         return agencia;
