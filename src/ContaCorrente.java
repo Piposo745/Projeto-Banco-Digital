5de6a6fb6d97d2d8d2d138a99be32f429ac66768
Piuavlr <63988968+Piposo745@users.noreply.github.com>
2021-12-29 16:20:15 -0300

    Att Ações  preço conta

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,12 +1,21 @@
+import java.util.Random;
+
 public class ContaCorrente extends Conta {
+
+    Random preco = new Random();
 
     private static int SEQUENCIAL = 1;
     public ContaCorrente() {
         super.agencia = AGENCIA_PADRAO;
         super.numeroConta = SEQUENCIAL++;
         super.saldo = 0.0f;
+        super.precoPetrobras = preco.nextFloat() * 40;
+        super.precoJBS = preco.nextFloat() * 30;
+        super.precoNubank = preco.nextFloat() * 80;
 
     }
+
+
 
 
     public void extrato(){
