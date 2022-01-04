18e784f9125519197d907979a1aac214a62ae09d
Piuavlr <63988968+Piposo745@users.noreply.github.com>
2022-01-04 14:48:01 -0300

    Update metodos abstratos

--- src/IConta.java
+++ src/IConta.java
@@ -1,10 +1,8 @@
 public interface IConta {
-     abstract void sacar(float valor);
-     abstract void depositar(float valor);
-     abstract void extrato();
-     abstract void taxaSacar();
-     abstract void taxaDepositar();
-     abstract void taxaConta();
-
-
+    abstract void sacar(float valor);
+    abstract void depositar(float valor);
+    abstract void extrato();
+    abstract void taxaSacar(float valor);
+    abstract void taxaDepositar();
+    abstract void taxaOperacoes();
 }
